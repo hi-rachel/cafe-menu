3f3ac5a5766ab4484497d170df9fe6d275d3cf98
hi-rachel <rachel.uiux@gmail.com>
2022-08-31 21:13:39 +0900

    Step2 - 요구사항 분석

--- src/js/index.js
+++ src/js/index.js
@@ -109,3 +109,28 @@ function App() {
 }
 
 App();
+
+// 🎯 step2 요구사항 - 상태 관리로 메뉴 관리하기
+
+// TODO localStorage Read & Write
+// - [ ] localStorage에 데이터를 저장한다.
+// - [ ] localStorage에 있는 데이터를 읽어온다.
+//(https://developer.mozilla.org/ko/docs/Web/API/Window/localStorage)
+
+// TODO 카테고리별 메뉴판 관리
+// - [ ] 에스프레소 메뉴판 관리
+// - [ ] 프라푸치노 메뉴판 관리
+// - [ ] 블렌디드 메뉴판 관리
+// - [ ] 티바나 메뉴판 관리
+// - [ ] 디저트 메뉴판 관리 (각각의 종류별로 관리할 수 있게 만든다.)
+
+// TODO 페이지 접근시 최초 데이터 Read & Rendering
+// - [ ] 페이지에 최초 로딩시 localStorage의 에스프레소 메뉴를 읽어온다.
+// - [ ] 에스프레소 메뉴를 페이지에 보여준다.
+
+// TODO 품절 관리
+// - [ ] 품절 상태인 경우를 보여줄 수 있게, 품절 버튼을 추가한다.
+// - [ ] 품절 버튼을 누르면 `sold-out` class를 추가하여 상태를 변경한다.
+// - [ ] 품절 버튼을 누르면 localStorage에 상태값이 저장된다.
+// - [ ] 품절 상태에서 버튼을 다시 한번 누르면 `sold-out` class가 제거된다.
+// - [ ] 품절 상태에서 버튼을 다시 한번 누르면 localStorage에 상태값이 저장된다.
